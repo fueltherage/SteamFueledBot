59beb61816d59b6e15b277ae7e41d258add497a1
fueltherage <wesleyallard1990@gmail.com>
2015-09-25 13:47:06 -0400

    Startup
    Initial project commit

---
+++ FueledSteamBot/FueledSteamBot/Bot/SteamBot.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SteamKit2;
+/*Steam Bot
+ * 
+ * Edited: Sep 25 2015
+ * Coder: Wes A.
+ * 
+ * Summary: This class is an automated bot
+ * It can sign into the the steam network, send friend invites,
+ * accept trades, and check items in the trade window.
+ * 
+ */
+
+
+
+namespace FueledSteamBot.Bot
+{
+    class SteamBot
+    {
+        public string botName = "DefaultBotName";
+
+        private string username;
+        public string BotUsername
+        {           
+            set { username = value; }
+        }
+        private string password;
+        public string BotPassword
+        {          
+            set { password = value; }
+        }
+        private bool running;
+        public bool Running
+        {
+            get { return running; }
+        }
+
+        private SteamClient client;
+        private CallbackManager cbManager; 
+        private SteamUser steamUser;  
+        
+        public SteamBot()
+        {            
+            running = true;
+
+            // create our steamclient instance
+            client = new SteamClient();
+            // create the callback manager which will route callbacks to function calls
+            cbManager = new CallbackManager(client);
+            // get the steamuser handler, which is used for logging on after successfully connecting    
+            steamUser = client.GetHandler<SteamUser>();
+
+            //Register Callbacks
+            cbManager.Subscribe<SteamClient.ConnectedCallback>(OnConnect);
+            cbManager.Subscribe<SteamClient.DisconnectedCallback>(OnDisconnect);
+
+            cbManager.Subscribe<SteamUser.LoggedOnCallback>(OnLogin);
+            //cbManager.Subscribe<SteamUser.LoggedOffCallback>(OnLoggedOff);
+                
+        }
+        public void Update()
+        {
+            cbManager.RunWaitCallbacks(TimeSpan.FromSeconds(1));
+        }
+        public void ConnectToSteamCloud()
+        {
+            bool emptyInfo = false;
+            if (username == null || username =="")
+            {
+                PrintError("ERROR: " + botName + " is missing username");
+                emptyInfo = true;
+            }
+            if (password == null || password == "")
+            {
+                PrintError("ERROR: " + botName + " is missing password");
+                emptyInfo = true;
+            }
+            if (emptyInfo)
+            {
+                //Abort Connecting
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Connecting...");
+                client.Connect();
+                running = true;
+            }
+        }
+
+        void OnConnect(SteamClient.ConnectedCallback callback)
+        {
+            if (callback.Result != EResult.OK)
+            {
+                PrintError("ERROR: "+callback.Result.ToString());
+                running = false;
+                return;
+            }
+            Console.WriteLine("Connected!");
+
+            steamUser.LogOn(new SteamUser.LogOnDetails {
+                Username = username,
+                Password = password,
+            });
+        }
+
+        void OnDisconnect(SteamClient.DisconnectedCallback callback)
+        {            
+            Console.WriteLine("Disconnected from steam cloud.");
+            running = false;
+            Console.WriteLine("Press any key to restart bot");
+            Console.ReadKey();
+        }
+
+        void OnLogin(SteamUser.LoggedOnCallback callback)
+        {
+            if (callback.Result != EResult.OK)
+            {
+                if (callback.Result == EResult.AccountLogonDenied)
+                {
+                    //SteamGuard is stopping login
+                }
+                PrintError(callback.Result.ToString());
+                PrintError(callback.ExtendedResult.ToString());
+                return;
+            }
+            Console.WriteLine("Login Successful");
+            //Login Success 
+        }
+
+        public void Destory()
+        {
+            steamUser.LogOff();
+            running = false;
+        }
+
+        static void PrintError(string _error)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine(_error);
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
---
+++ FueledSteamBot/FueledSteamBot/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SteamKit2;
+
+namespace FueledSteamBot
+{
+    class Program
+    {
+        static bool SteamBotRunning = true;
+
+        static void Main(string[] args)
+        {
+            Console.WindowHeight = 30;           
+            Console.Title = "Steam Fueled Bot";
+            Bot.SteamBot bot = new Bot.SteamBot();
+
+
+            while (SteamBotRunning)
+            {
+                Console.Clear();
+                Banner();              
+
+                //Get the bot's username and password   
+                string username, password;
+                Console.Write("Enter the bot's steam\nusername: ");
+                username = Console.ReadLine();
+                Console.Write("password: ");
+                password = Console.ReadLine();
+
+                bot.BotUsername = username;
+                bot.BotPassword = password;
+
+                //bot.BotUsername = "steamfueledbot";
+                //bot.BotPassword = "D1-fu&bwc";
+                
+                bot.ConnectToSteamCloud();
+
+                Console.CancelKeyPress += (s, e) =>
+                {
+                    e.Cancel = true;
+
+                    Console.WriteLine("Received {0}, disconnecting...", e.SpecialKey);
+                    bot.Destory();
+                };
+
+                while (bot.Running)
+                {
+                    bot.Update();
+                }
+                bot.Destory();
+            }
+        }
+        static void Banner()
+        {
+            //Banner
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(" _______ _________ _______  _______  _______");
+            Console.WriteLine("(  ____ \\\\__   __/(  ____ \\(  ___  )(       )");
+            Console.WriteLine("| (    \\/   ) (   | (    \\/| (   ) || () () |");
+            Console.WriteLine("| (_____    | |   | (__    | (___) || || || |");
+            Console.WriteLine("(_____  )   | |   |  __)   |  ___  || |(_)| |");
+            Console.WriteLine("      ) |   | |   | (      | (   ) || |   | |");
+            Console.WriteLine("/\\____) |   | |   | (____/\\| )   ( || )   ( |");
+            Console.WriteLine("\\_______)   )_(   (_______/|/     \\||/     \\|");
+            Console.WriteLine("");
+            Console.WriteLine(" _______           _______  _        _______  ______ ");
+            Console.WriteLine("(  ____ \\|\\     /|(  ____ \\( \\      (  ____ \\(  __  \\");
+            Console.WriteLine("| (    \\/| )   ( || (    \\/| (      | (    \\/| (  \\  )");
+            Console.WriteLine("| (__    | |   | || (__    | |      | (__    | |   ) |");
+            Console.WriteLine("|  __)   | |   | ||  __)   | |      |  __)   | |   | |");
+            Console.WriteLine("| (      | |   | || (      | |      | (      | |   ) |");
+            Console.WriteLine("| )      | (___) || (____/\\| (____/\\| (____/\\| (__/  )");
+            Console.WriteLine("|/       (_______)(_______/(_______/(_______/(______/");
+            Console.WriteLine("");
+            Console.WriteLine(" ______   _______ _________");
+            Console.WriteLine("(  ___ \\ (  ___  )\\__   __/");
+            Console.WriteLine("| (   ) )| (   ) |   ) (");
+            Console.WriteLine("| (__/ / | |   | |   | |");
+            Console.WriteLine("|  __ (  | |   | |   | |");
+            Console.WriteLine("| (  \\ \\ | |   | |   | |");
+            Console.WriteLine("| )___) )| (___) |   | |");
+            Console.WriteLine("|/ \\___/ (_______)   )_(\n");
+            Console.ForegroundColor = ConsoleColor.White;
+            //EndBanner
+        }
+        
+    }
+
+}
